2deecd815cb0c015a77310e091ae2182cf0e2183
nicolasculerier <nicolas.culerier@laposte.net>
2018-07-26 15:26:54 +0200

    Exo 1 terminé

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |doctor|
+  doctor = Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty: Faker::Gender.binary_type, postal_code: Faker::Address.zip_code)
+end
+
+10.times do |patient|
+  patient = Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
+10.times do |appointment|
+  appointment = Appointment.create(date: Faker::Date.between(2.days.ago, Date.today), doctor_id: rand(Doctor.first.id..Doctor.last.id), patient_id: rand(Patient.first.id..Patient.last.id) )
+end
